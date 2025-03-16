0d27b37fc00b2ff32b5b3eae66c430227c154aae
IgorJanuszkiewicz <igorjanuszkiewicz997@gmail.com>
2025-03-16 15:54:42 +0100

    Fix auth service mock

--- lib/features/auth/auth_service.dart
+++ lib/features/auth/auth_service.dart
@@ -21,6 +21,7 @@ abstract class AuthService {
 
   Future<SignInResult> signInWithEmail(String email, String password);
 
+  // Returns error message in case of error, null otherwise
   Future<String?> signUpWithEmail(
     String email,
     String password,
@@ -91,7 +92,7 @@ class AuthServiceMock implements AuthService {
         name: name,
         email: email);
     _users[email] = newUser;
-    return 'User registered successfully';
+    return null;
   }
 
   @override
